cee97bbefe74cf6ef7e51da4bea8bcdb5880a0a3
yejiahn1 <yeji_09@naver.com>
2024-11-26 14:34:42 +0900

    주석정리

--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -1,57 +1,52 @@
-"use client";
+'use client';
 
-import { ADD_TODO, DELETE_TODO, setTodos, UPDATE_TODO } from "@/states/todoReducer";
-import { createContext, useContext, useEffect, useReducer } from "react";
+import { ADD_TODO, DELETE_TODO, setTodos, UPDATE_TODO } from '@/states/todoReducer';
+import { createContext, useContext, useEffect, useReducer } from 'react';
 
-// 생성
 const TodoContext = createContext();
 
-// 공급
-export const TodoProvider = ({children}) => {
-  const [todos, dispatch] = useReducer(setTodos, [])
-  // 로컬 스토리지 키 선언
-  const LOCAL_STORAGE_KEY = "my-todo-app-todos";
+export const TodoProvider = ({ children }) => {
+    const [todos, dispatch] = useReducer(setTodos, []);
 
-  useEffect(() => {
-      const savedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
-      // setTodos(savedTodos);
-      savedTodos.forEach((todo) => {return dispatch({
-        type: ADD_TODO,
-        payload: todo
-      })})
-  }, [])
+    const LOCAL_STORAGE_KEY = 'my-todo-app-todos';
 
-  useEffect(() => {
-      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
-  }, [todos])
-  
-  const addTodo = (task) => {
-    dispatch({type: ADD_TODO, payload: {task}})
-  }
+    useEffect(() => {
+        const savedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
+        // setTodos(savedTodos);
+        savedTodos.forEach((todo) => {
+            return dispatch({
+                type: ADD_TODO,
+                payload: todo,
+            });
+        });
+    }, []);
 
-  const onUpdate = (id) => {
-    dispatch({type: UPDATE_TODO, payload: {id}})
-  }
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+    }, [todos]);
 
-  const onDelete = (id) => {
-    dispatch({type: DELETE_TODO, payload: {id}})
-  }
+    const addTodo = (task) => {
+        dispatch({ type: ADD_TODO, payload: { task } });
+    };
 
-  const value = {
-    todos,
-    addTodo,
-    onUpdate,
-    onDelete
-  }
+    const onUpdate = (id) => {
+        dispatch({ type: UPDATE_TODO, payload: { id } });
+    };
 
-  return (
-    <TodoContext.Provider value={value}>
-      {children}
-    </TodoContext.Provider>
-  )
-}
+    const onDelete = (id) => {
+        dispatch({ type: DELETE_TODO, payload: { id } });
+    };
 
-// 사용
+    const value = {
+        todos,
+        addTodo,
+        onUpdate,
+        onDelete,
+    };
+
+    return <TodoContext.Provider value={value}>{children}</TodoContext.Provider>;
+};
+
 export const useTodo = () => {
-  return useContext(TodoContext)
-}
+    return useContext(TodoContext);
+};
